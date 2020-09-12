bb40fca5c372068ada7a4be82608cf357df1ed6e
felipe.xavier <felipe.xavier@funceme.br>
2020-09-11 21:54:37 -0300

    Update README file and example 2.10

--- 2.10-ratings-counter.py
+++ 2.10-ratings-counter.py
@@ -1,16 +1,25 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType
+
 
 spark = SparkSession.builder.appName('RatingsHistogram').getOrCreate()
+schema = StructType([
+    StructField("user_id", IntegerType(), True),
+    StructField("movie_id", IntegerType(), True),
+    StructField("rating", IntegerType(), True),
+    StructField("timestamp", IntegerType(), True)
+])
+ratings_df = spark.read.csv('/data/courses/SparkCourse/ml-100k/u.data',
+                            sep='\t',
+                            schema=schema)
 
-ratings_df = spark.read.csv('/data/courses/SparkCourse/ml-100k/u.data', sep='\t')
-
-print(ratings_df.printSchema())
+ratings_df.printSchema()
 ratings_df.show()
 
 hist_df = ratings_df\
-    .groupby('_c2')\
+    .groupby('rating')\
     .count()\
     .orderBy('count', ascending=False)\
     .cache()
 
-print(hist_df.show())
+hist_df.show()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PySpark3-Examples
 Code examples from the Udemy "Taming Big Data with Apache Spark and Python - Hands On!" course by Frank Kane updated to PySpark3 using Dataframes.
+
+The course was first given using Spark2, later professor 
+Kane added some lectures about Spark3 and Dataframes, 
+but the code examples still use RDDs. So the goal here 
+is to provide updated examples using Dataframes to help you advance your studies.
+
+All the data files are given in the lectures and are stored locally at /data/courses/SparkCourse/
+
+Once you install Java and PySpark and set the environment variables you will be good running these codes.
+
+
